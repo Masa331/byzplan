378ed7383ea7b85001fec00dc021423bfdee72f4
Masa331 <pdonat@seznam.cz>
2013-07-30 18:25:49 +0200

    precompiling pdf.css.scss

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module Byzplan
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['pdf.css.scss']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
