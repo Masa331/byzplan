1885a2a163d0e11e7826b3c58c3f92c822adff9b
Masa331 <pdonat@seznam.cz>
2013-07-30 10:28:41 +0200

    menena cesta k wkhtmltopdf

--- config/initializers/WickedPdf.rb
+++ config/initializers/WickedPdf.rb
@@ -1,1 +1,3 @@
-WickedPdf.config = {exe_path: '~/app/shared/bundle/ruby/1.9.1/bin/wkhtmltopdf'}
+if Rails.env.production?
+	WickedPdf.config = {exe_path: '/usr/home/byzplancz/app/shared/bundle/ruby/1.9.1/bin/wkhtmltopdf'}
+end
