379d6927ae12c881f3d2b56ef1c9befd30cc9018
Masa331 <pdonat@seznam.cz>
2013-08-06 20:04:04 +0200

    trying another middleware for serving gziped and cached assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,20 +11,15 @@ Byzplan::Application.configure do
 
   # Full error reports are disabled and carails .htaccessching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  # Tohle predtim bylo false!!!
-  config.serve_static_assets = true
-
-  config.static_cache_control = "public, max-age=#{1.year.to_i}"
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  # Tohle bylo taky false
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
